0e013ea183efebe3c100429110bdfd43fc233f5c
Jack Jain <cirocelot@gmail.com>
2018-10-01 14:20:32 -0600

    First commit

---
+++ varibles-and-data.js
@@ -0,0 +1,21 @@
+/*
+ * Varibles, Data Types, and Typing
+ */
+let wordData = "Word List";
+
+console.log(wordData);
+
+let subTotal = 100;
+let tax = subTotal * 0.07;
+let totalWithTax = subTotal + tax
+
+let obj = {
+	key1: "value",
+	key2: 4,
+	boolean: true,
+	obj2: {
+		obj2key1: "Internal Object Value",
+	}
+};
+
+console.log(obj.key1);
